29d0ac0d6c64b32af492694bd2136ec686fda8a2
Michael <michaelkallies88@gmail.com>
2018-09-20 16:41:03 -0400

    updates

--- jest/setupTests.js
+++ jest/setupTests.js
@@ -1,6 +1,8 @@
 import { configure, shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
+// Add other globals for easier testing
+
 global.shallow = shallow
 global.mount = mount
 
